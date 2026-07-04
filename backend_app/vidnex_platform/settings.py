2cd73593a663a2d2b86dddffb2bb903c9b89c8c7
18me2meetghorecha-gif <18me2.meet.ghorecha@gmail.com>
2026-07-04 21:04:27 +0530

    Add Railway deployment config (railway.json, nixpacks.toml)

--- backend_app/vidnex_platform/settings.py
+++ backend_app/vidnex_platform/settings.py
@@ -141,7 +141,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR.parent / 'assets',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
